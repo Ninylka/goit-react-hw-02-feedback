fd4d7e025dc70c8463fc999cec6988c425b6ebef
Ninylka <shemetylka@gmail.com>
2023-11-07 17:41:45 +0100

    second

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,80 @@
+import {  Section } from '../Section/Section'
+import { FeedbackOptions } from '../Feedbackoptions/FeedbackOptions'
+import { Statistics } from '../Statistics/Statistics'
+import { Notification } from '../Notification/Notification'
+import styled from 'styled-components'
+
+
+const { Component } = require("react")
+
+ export class CouterFeedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    }
+    handleLeaveFeedback = (type) => {
+        this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
+      };
+   
+    handlerGoodClick = () => {
+        this.setState((prevState) => ({ good: prevState.good + 1}))
+      
+    }
+    handlerNeutralClick = () => {
+        this.setState((prevState) => ({ neutral: prevState.neutral+ 1}))
+      
+    }
+
+    handlerBadClick = () => {
+        this.setState((prevState) => ({ bad: prevState.bad + 1}))
+      
+    }
+
+
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        return good + neutral + bad;
+      };
+    
+      countPositiveFeedbackPercentage = () => {
+        const total = this.countTotalFeedback();
+        const { good } = this.state;
+        return total > 0 ? Math.round((good * 100) / total) + '%' : '0%';
+      };
+    
+
+    render() {
+         
+        const { good, neutral, bad } = this.state;
+        const total = this.countTotalFeedback();
+        const positivePercentage = this.countPositiveFeedbackPercentage();
+        const options = Object.keys(this.state);
+    
+        return(
+          
+            <ContainerFeedback>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={options} onLeaveFeedback={this.handleLeaveFeedback} />
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />
+          ) : (<Notification message="There is no feedback" />)
+          }
+        </Section>
+      </ContainerFeedback>
+        )
+    }
+}
+
+export const ContainerFeedback = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ flex-direction: column;
+`
+
+
+
+
